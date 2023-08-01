4f244a30be3a6cdee0b2f958d42d3ca396f964bb
Michael Gichuhi <gmikekamau@gmail.com>
2023-08-01 16:37:11 +0300

    first flutter project

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'flutter-app',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('welcome home'),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text('hello world'),
+      ),
+    );
+  }
+}
